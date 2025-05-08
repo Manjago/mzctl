4c5fa690dd044071f9a262c79dd250dc32f97477
Kirill Temnenkov <temnenkov@yandex.ru>
2025-05-08 21:54:50 +0300

    cool report

--- src/test/java/com/temnenkov/mzctl/generation/CellularAutomataMazeGeneratorTest.java
+++ src/test/java/com/temnenkov/mzctl/generation/CellularAutomataMazeGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.temnenkov.mzctl.generation;
 
+import com.temnenkov.mzctl.analysis.CellularAutomataMazeAnalyzer;
 import com.temnenkov.mzctl.model.CellularAutomataMaze;
 import com.temnenkov.mzctl.model.MazeDim;
 import com.temnenkov.mzctl.visualization.CellularAutomataAsciiVisualizer;
@@ -24,6 +25,9 @@ class CellularAutomataMazeGeneratorTest {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+
+            report(maze);
+
             System.out.println();
         };
 
@@ -31,5 +35,17 @@ class CellularAutomataMazeGeneratorTest {
                0.75, 10, 0.5, 0.625, new Random(42L), visualizer);
         final CellularAutomataMaze maze = mazeGenerator.generate(MazeDim.of(10, 10));
         assertNotNull(maze);
+
+        report(maze);
+    }
+
+    private static void report(CellularAutomataMaze maze) {
+        final CellularAutomataMazeAnalyzer cellularAutomataMazeAnalyzer = new CellularAutomataMazeAnalyzer(maze);
+        final int isolatedAreas = cellularAutomataMazeAnalyzer.countIsolatedAreas();
+        final int largestAreaSize = cellularAutomataMazeAnalyzer.largestAreaSize();
+        final double wallPercentage = cellularAutomataMazeAnalyzer.wallPercentage() * 100.0; // <-- в процентах;
+        System.out.printf("Isolated areas: %d%n", isolatedAreas);
+        System.out.printf("Largest areas size: %d%n", largestAreaSize);
+        System.out.printf("Wall percentage: %.2f%%%n", wallPercentage); // <-- знак процента
     }
 }
