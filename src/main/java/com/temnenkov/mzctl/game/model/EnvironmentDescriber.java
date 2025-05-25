85f5c68fe2f1944424e67823d25d2910c8b560f7
Kirill Temnenkov <temnenkov@yandex.ru>
2025-05-25 17:47:24 +0300

    fix(engine): исправить сериализацию PlayerSession

--- src/main/java/com/temnenkov/mzctl/game/model/EnvironmentDescriber.java
+++ src/main/java/com/temnenkov/mzctl/game/model/EnvironmentDescriber.java
@@ -1,7 +1,14 @@
 package com.temnenkov.mzctl.game.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.jetbrains.annotations.NotNull;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = MazeEnvironmentDescriber.class, name = "maze"),
+        @JsonSubTypes.Type(value = ShortEnvironmentDescriber.class, name = "short")
+})
 public interface EnvironmentDescriber {
     @NotNull String describeEnvironment(@NotNull PlayerStateND player);
 }
